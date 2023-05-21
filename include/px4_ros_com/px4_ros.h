5588c2b1b0ae047dc74abbbcc5372bc1692fcebb
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-05-21 16:36:49 +0300

    Add publisher for veisual odom

--- include/px4_ros_com/px4_ros.h
+++ include/px4_ros_com/px4_ros.h
@@ -16,6 +16,8 @@
 // #include <px4_msgs/msg/trajectory_setpoint.hpp>
 #include <px4_ros_com/frame_transforms.h> /*Transform conversions*/
 
+#include <nav_msgs/msg/odometry.hpp> // ROS odom -> PX4 odom
+
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
@@ -39,18 +41,27 @@ private:
 
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_; /**< Publisher for Odometry ROS msg  */
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub_; /**< Publisher for pose stamped ROS msg  */
+    rclcpp::Publisher<px4_msgs::msg::VehicleOdometry>::SharedPtr vis_odom_pub_; /**< Publisher for fmu/in/vehicle_visual_odometry */
 
     // rclcpp::Publisher<px4_msgs::msg::VehicleAttitudeSetpoint>::SharedPtr att_sp_pub_; /**< Publishes to px4 fmu/in/vehicle_attitude_setpoint topic  */
     // rclcpp::Publisher<px4_msgs::msg::VehicleRatesSetpoint>::SharedPtr rates_sp_pub_; /**< Publishes to px4 fmu/in/vehicle_rates_setpoint  */
     // rclcpp::Publisher<px4_msgs::msg::TrajectorySetpoint>::SharedPtr trajectory_sp_pub_; /**< Publishes to px4 fmu/in/trajectory_setpoint  */
 
     rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr px4_odom_sub_;/**< Subscriber to px4 odometry. */
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr ros_vis_odom_sub_;/**< Subscriber to ROS visual odometry. */
 
     /**
      * @brief Callback for PX4 odom. Converts PX4 odom to ROS odom
      * @param msg [px4_msgs::msg::VehicleOdometry] 
     */
     void px4OdomCallback(const px4_msgs::msg::VehicleOdometry & msg);
+
+    /**
+     * @brief Converts ROS odom topic to PX4 visual odom topic.
+     *  typically used to inject visual odometry info
+     * @param msg [nav_msgs::msg::Odometry] 
+    */
+    void rosVisualOdomCallback(const nav_msgs::msg::Odometry & msg);
 };
 
 /**
@@ -79,11 +90,15 @@ PX4ROS::PX4ROS(/* args */): Node("px4_ros")
    
     odom_pub_ =  this->create_publisher<nav_msgs::msg::Odometry>("px4_ros/odom", 10);
     pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("px4_ros/pose", 10);
+    vis_odom_pub_ = this->create_publisher<px4_msgs::msg::VehicleOdometry>("fmu/in/vehicle_visual_odometry", 10);
 
     rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
 		auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
     px4_odom_sub_ = this->create_subscription<px4_msgs::msg::VehicleOdometry>(
     "fmu/out/vehicle_odometry", qos, std::bind(&PX4ROS::px4OdomCallback, this, _1));
+
+    ros_vis_odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+    "vio/ros_odom", qos, std::bind(&PX4ROS::rosVisualOdomCallback, this, _1));
 
 }
 
@@ -183,6 +198,91 @@ PX4ROS::px4OdomCallback(const px4_msgs::msg::VehicleOdometry & msg)
         t.transform.rotation.z = ros_pose_msg_.pose.orientation.z;
         tf_broadcaster_->sendTransform(t);
     }
+}
+
+void
+PX4ROS::rosVisualOdomCallback(const nav_msgs::msg::Odometry & msg)
+{
+    px4_msgs::msg::VehicleOdometry px4_odom_msg;
+    px4_odom_msg.timestamp = 0; //static_cast<uint64_t>(msg.header.stamp.sec*1e6) + static_cast<uint64_t>(msg.header.stamp.nanosec/1e3);
+    px4_odom_msg.timestamp_sample = 0;
+
+    px4_odom_msg.pose_frame = px4_odom_msg.POSE_FRAME_NED;
+    px4_odom_msg.velocity_frame = px4_odom_msg.VELOCITY_FRAME_NED;
+
+    // position
+    Eigen::Vector3d position_enu(msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z);
+    Eigen::Vector3d position_ned = enu_to_ned_local_frame(position_enu);
+    // Velocity
+    Eigen::Vector3d velocity_enu(msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z);
+    Eigen::Vector3d velocity_ned = enu_to_ned_local_frame(velocity_enu);
+    // Orientation
+    Eigen::Quaterniond q_enu(msg.pose.pose.orientation.w,
+                            msg.pose.pose.orientation.x,
+                            msg.pose.pose.orientation.y,
+                            msg.pose.pose.orientation.z);
+    Eigen::Quaterniond q_ned = ros_to_px4_orientation(q_enu);
+
+    // Angular Velocity
+    Eigen::Vector3d angular_vel_ros(msg.twist.twist.angular.x, msg.twist.twist.angular.y, msg.twist.twist.angular.z);
+    Eigen::Vector3d angular_vel_px4 = baselink_to_aircraft_body_frame(angular_vel_ros);
+    
+
+    // Pose covariance
+    Covariance3d cov_pos_ros = {
+        msg.pose.covariance[0], msg.pose.covariance[1], msg.pose.covariance[2],
+        msg.pose.covariance[6], msg.pose.covariance[7], msg.pose.covariance[8],
+        msg.pose.covariance[12], msg.pose.covariance[13], msg.pose.covariance[14]
+    };
+
+    Covariance3d cov_rot_ros = {
+        msg.pose.covariance[21], msg.pose.covariance[22], msg.pose.covariance[23],
+        msg.pose.covariance[27], msg.pose.covariance[28], msg.pose.covariance[29],
+        msg.pose.covariance[33], msg.pose.covariance[34], msg.pose.covariance[35]
+    };
+
+    // Velocity covariance
+
+    Covariance3d cov_vel_ros = {
+        msg.twist.covariance[0], msg.twist.covariance[1], msg.twist.covariance[2],
+        msg.twist.covariance[6], msg.twist.covariance[7], msg.twist.covariance[8],
+        msg.twist.covariance[12], msg.twist.covariance[13], msg.twist.covariance[14]
+    };
+
+    Covariance3d cov_pos_px4 = transform_static_frame(cov_pos_ros, StaticTF::ENU_TO_NED);
+    Covariance3d cov_vel_px4 = transform_static_frame(cov_vel_ros, StaticTF::ENU_TO_NED);
+    Covariance3d cov_rot_px4 = transform_frame(cov_rot_ros, q_ned);
+
+    px4_odom_msg.position[0] = position_ned.x();
+    px4_odom_msg.position[1] = position_ned.y();
+    px4_odom_msg.position[2] = position_ned.z();
+
+    px4_odom_msg.velocity[0] = velocity_ned.x();
+    px4_odom_msg.velocity[1] = velocity_ned.y();
+    px4_odom_msg.velocity[2] = velocity_ned.z();
+    
+    px4_odom_msg.q[0] = q_ned.w();
+    px4_odom_msg.q[1] = q_ned.x();
+    px4_odom_msg.q[2] = q_ned.y();
+    px4_odom_msg.q[3] = q_ned.z();
+
+    px4_odom_msg.angular_velocity[0] = angular_vel_px4.x();
+    px4_odom_msg.angular_velocity[1] = angular_vel_px4.y();
+    px4_odom_msg.angular_velocity[2] = angular_vel_px4.z();
+
+    px4_odom_msg.position_variance[0] = cov_pos_px4[0]; // diagonal elements
+    px4_odom_msg.position_variance[1] = cov_pos_px4[4];
+    px4_odom_msg.position_variance[2] = cov_pos_px4[8];
+
+    px4_odom_msg.velocity_variance[0] = cov_vel_px4[0];
+    px4_odom_msg.velocity_variance[1] = cov_vel_px4[4];
+    px4_odom_msg.velocity_variance[2] = cov_vel_px4[8];
+
+    px4_odom_msg.orientation_variance[0] = cov_rot_px4[0];
+    px4_odom_msg.orientation_variance[1] = cov_rot_px4[4];
+    px4_odom_msg.orientation_variance[2] = cov_rot_px4[8];
+    
+    vis_odom_pub_->publish(px4_odom_msg);
 }
 
 #endif
